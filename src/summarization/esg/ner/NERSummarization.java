f1a51eb0b201eec90a0c070ab7fa32c0de95063a
rushdishams <rushdecoder@gmail.com>
2015-12-16 19:47:46 +0000

    v-0.1.-5: some ordering is done with NER weighting

--- src/summarization/esg/ner/NERSummarization.java
+++ src/summarization/esg/ner/NERSummarization.java
@@ -1,41 +1,49 @@
 package summarization.esg.ner;
+
 import edu.stanford.nlp.ie.AbstractSequenceClassifier;
 import edu.stanford.nlp.ie.crf.*;
 import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.util.ArrayUtils;
 import edu.stanford.nlp.util.Triple;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import org.apache.commons.io.FileUtils;
 
 import com.boundary.sentence.TextContent;
 
-
-/** This is a demo of calling CRFClassifier programmatically.
- *  <p>
- *  Usage: {@code java -mx400m -cp "*" NERDemo [serializedClassifier [fileName]] }
- *  <p>
- *  If arguments aren't specified, they default to
- *  classifiers/english.all.3class.distsim.crf.ser.gz and some hardcoded sample text.
- *  If run with arguments, it shows some of the ways to get k-best labelings and
- *  probabilities out with CRFClassifier. If run without arguments, it shows some of
- *  the alternative output formats that you can get.
- *  <p>
- *  To use CRFClassifier from the command line:
- *  </p><blockquote>
- *  {@code java -mx400m edu.stanford.nlp.ie.crf.CRFClassifier -loadClassifier [classifier] -textFile [file] }
- *  </blockquote><p>
- *  Or if the file is already tokenized and one word per line, perhaps in
- *  a tab-separated value format with extra columns for part-of-speech tag,
- *  etc., use the version below (note the 's' instead of the 'x'):
- *  </p><blockquote>
- *  {@code java -mx400m edu.stanford.nlp.ie.crf.CRFClassifier -loadClassifier [classifier] -testFile [file] }
- *  </blockquote>
+/**
+ * This is a demo of calling CRFClassifier programmatically.
+ * <p>
+ * Usage:
+ * {@code java -mx400m -cp "*" NERDemo [serializedClassifier [fileName]] }
+ * <p>
+ * If arguments aren't specified, they default to
+ * classifiers/english.all.3class.distsim.crf.ser.gz and some hardcoded sample
+ * text. If run with arguments, it shows some of the ways to get k-best
+ * labelings and probabilities out with CRFClassifier. If run without arguments,
+ * it shows some of the alternative output formats that you can get.
+ * <p>
+ * To use CRFClassifier from the command line:
+ * </p>
+ * <blockquote>
+ * {@code java -mx400m edu.stanford.nlp.ie.crf.CRFClassifier -loadClassifier [classifier] -textFile [file] }
+ * </blockquote>
+ * <p>
+ * Or if the file is already tokenized and one word per line, perhaps in a
+ * tab-separated value format with extra columns for part-of-speech tag, etc.,
+ * use the version below (note the 's' instead of the 'x'):
+ * </p>
+ * <blockquote>
+ * {@code java -mx400m edu.stanford.nlp.ie.crf.CRFClassifier -loadClassifier [classifier] -testFile [file] }
+ * </blockquote>
  *
- *  @author Jenny Finkel
- *  @author Christopher Manning
+ * @author Jenny Finkel
+ * @author Christopher Manning
  */
 
 public class NERSummarization {
@@ -44,9 +52,9 @@ public class NERSummarization {
 	private static AbstractSequenceClassifier<CoreLabel> classifier;
 	private File[] listOfFiles;
 	private String[] articleContents;
-	private String[] featureVector;
+	private int[] groupOne, groupTwo;
 
-	public NERSummarization(String folderPath){
+	public NERSummarization(String folderPath) {
 		try {
 			classifier = CRFClassifier.getClassifier(SEVEN_CLASS_CLASSIFIER);
 		} catch (ClassCastException | ClassNotFoundException | IOException e) {
@@ -57,9 +65,9 @@ public class NERSummarization {
 		articleContents = new String[listOfFiles.length];
 	}
 
-	public void parseFiles(){
+	public void parseFiles() {
 
-		for(int i = 0; i < listOfFiles.length; i ++){
+		for (int i = 0; i < listOfFiles.length; i++) {
 			try {
 				articleContents[i] = FileUtils.readFileToString(listOfFiles[i]);
 			} catch (IOException e) {
@@ -67,75 +75,108 @@ public class NERSummarization {
 			}
 		}
 	}
-	
-	public String[] getArticleContents(){
+
+	public String[] getArticleContents() {
 		return articleContents;
 	}
-	
-	public String[] extractSentence(String content){
-		TextContent t = new TextContent(); //creating TextContent object
+
+	public String[] extractSentence(String content) {
+		TextContent t = new TextContent(); // creating TextContent object
 		t.setText(content);
 		t.setSentenceBoundary();
 		String[] sentences = t.getSentence();
 		return sentences;
 	}
-	
-	public void extractNER(String[] articleSentences){
+
+	public void extractNER(String[] articleSentences) {
+
+		groupOne = new int[articleSentences.length];
+		groupTwo = new int[articleSentences.length];
 		int j = 0;
 		String first = "", second = "", none = "";
 		for (String sentence : articleSentences) {
-			boolean foundFirst = false, foundSecond = false, foundNone = true;
+			groupOne[j] = 0;
+			groupTwo[j] = 0;
+
+			List<Triple<String, Integer, Integer>> triples = classifier.classifyToCharacterOffsets(sentence);
+			
+			System.out.println(triples.toString());
+			for (Triple<String, Integer, Integer> trip : triples) {
+
+				if (trip.asList().contains("MONEY") || trip.asList().contains("PERCENT")) {
+					groupTwo[j]++;
+				}
+				if (trip.asList().contains("ORGANIZATION") || trip.asList().contains("PERSON")
+						|| trip.asList().contains("LOCATION") || trip.asList().contains("DATE")
+						|| trip.asList().contains("TIME")) {
+					groupOne[j]++;
+				}
+
+			}
 			j++;
-			List<Triple<String,Integer,Integer>> triples = classifier.classifyToCharacterOffsets(sentence);
-			System.out.println(sentence);
-			for (int i = 0; i < triples.size(); i++){
-				System.out.print (triples.get(i) + " ");
-			}
-			System.out.println("\n");
-			
-			for (Triple<String,Integer,Integer> trip : triples) {
-				
-			
-					foundNone = false;
-				
-//				System.out.println(trip.asList().toString());
-				if(trip.asList().contains("MONEY") || trip.asList().contains("PERCENT")){
-					foundSecond = true;
-//					break;
-				}
-				else{
-					foundFirst = true;
-				}
-				
-			}
-			if(foundNone){
-				none += sentence;
-			}
-			else if(foundSecond){
-				
-				second += sentence + "\n";
-			}
-			else{
-				first += sentence + "\n";
-			}
-	/*		for (Triple<String,Integer,Integer> trip : triples) {
-				System.out.printf("%s over character offsets [%d, %d) in sentence %d.%n",
-						trip.first(), trip.second(), trip.third, j); 
-				System.out.println(str.substring(trip.second(), trip.third));
-				
-			}*/
 		}
-		System.out.println("===========FIRST================\n" + first);
-		System.out.println("===========SECOND================\n" + second);
+		for(int i = 0; i < groupOne.length; i++){
+			System.out.print(groupOne[i] + " ");
+		}
 		System.out.println("---");
+		for(int i = 0; i < groupOne.length; i++){
+			System.out.print(groupTwo[i] + " ");
+		}
+		System.out.println("---");
+		int length = groupOne.length;
+		for(int i = 0; i < groupOne.length; i++){
+			groupOne[i] = groupOne[i]* (length - i);
+		}
+		for(int i = 0; i < groupOne.length; i++){
+			groupTwo[i] = groupTwo[i]* (length - i);
+		}
+		
+		
+		for(int i = 0; i < groupOne.length; i++){
+			System.out.print(groupOne[i] + "\t");
+		}
+		System.out.println("---");
+		for(int i = 0; i < groupOne.length; i++){
+			System.out.print(groupTwo[i] + "\t");
+		}
+		int[] finalGroupOne = new int[groupOne.length];
+		int[] finalGroupTwo = new int[groupTwo.length];
+		for(int i = 0; i < 5; i++){
+			int max = Arrays.stream(groupOne).max().getAsInt();
+			for (int x = 0; x < groupOne.length; x++){
+				if(groupOne[x] == max){
+					groupOne[x] = 0;
+					finalGroupOne[x] = max;
+				}
+			}
+			
+			max = Arrays.stream(groupTwo).max().getAsInt();
+			for (int x = 0; x < groupTwo.length; x++){
+				if(groupTwo[x] == max){
+					groupTwo[x] = 0;
+					finalGroupTwo[x] = max;
+				}
+			}
+		}
+		System.out.println("\n");
+		for (int i = 0; i <  finalGroupOne.length; i++){
+			System.out.print(finalGroupOne[i] + "\t");
+		}
+		System.out.println("\n");
+
+		for (int i = 0; i <  finalGroupOne.length; i++){
+			System.out.print(finalGroupTwo[i] + "\t");
+		}
+
+		
 	}
-	
+
 	public static void main(String[] args) throws Exception {
 
 		NERSummarization nerSum = new NERSummarization(args[0]);
 		nerSum.parseFiles();
 		String[] documents = nerSum.getArticleContents();
-		for(String document: documents){
+		for (String document : documents) {
 			String[] sentences = nerSum.extractSentence(document);
 			nerSum.extractNER(sentences);
 		}
